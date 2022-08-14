8328a504c9939dc58184294d37be054942587353
snponamgi <97530913+snponamgi@users.noreply.github.com>
2022-08-14 16:14:05 -0400

    Updated by CrossUI No-Code App Builder

--- phpClass/config.php
+++ phpClass/config.php
@@ -1,5 +1,5 @@
 <?php
-    @define('DB_HOST', 'localhost');
+    @define('DB_HOST', 'localhost:3306');
     @define('DB_USER', 'root');
     @define('DB_PASS', '$!mr!nski');
     @define('DB_DBNAME', 'xui_ordermanagement');
